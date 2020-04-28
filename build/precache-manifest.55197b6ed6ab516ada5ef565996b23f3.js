8423d57611ce7c4449fc3e3b8052aa3b5d4a5f2b
Humaid Desai <43137815+Humaid-0501@users.noreply.github.com>
2020-04-29 01:00:23 +0530

    Host initial

---
+++ build/precache-manifest.55197b6ed6ab516ada5ef565996b23f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2698d079d666a39aff54cc6ac5781c",
+    "url": "/.github.io/Court/index.html"
+  },
+  {
+    "revision": "7014f2c786197a81e08a",
+    "url": "/.github.io/Court/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ec7b8f8b29515d1dd5e7",
+    "url": "/.github.io/Court/static/css/main.1adba427.chunk.css"
+  },
+  {
+    "revision": "7014f2c786197a81e08a",
+    "url": "/.github.io/Court/static/js/2.2b8cdd13.chunk.js"
+  },
+  {
+    "revision": "5c2176e7db1837929debf21c65634f23",
+    "url": "/.github.io/Court/static/js/2.2b8cdd13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec7b8f8b29515d1dd5e7",
+    "url": "/.github.io/Court/static/js/main.d6e2d488.chunk.js"
+  },
+  {
+    "revision": "e6a56bba11686d16584c",
+    "url": "/.github.io/Court/static/js/runtime-main.0b3f46b2.js"
+  }
+]);
